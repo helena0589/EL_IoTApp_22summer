84e52dcc8a74111735382ee6196c57163342800f
Huiwoo Jo <84004687+helena0589@users.noreply.github.com>
2022-08-21 22:35:19 +0900

    소스코드 수정
    - 소스코드 주석 수정
    - 소스코드 파일명 변경
    - 라이브러리 파일 추가

--- 아두이노 소스코드/Air_EL_IoTApp/Air_EL_IoTApp.ino
+++ 아두이노 소스코드/Air_EL_IoTApp/Air_EL_IoTApp.ino
@@ -1,6 +1,38 @@
+/* 
+ * 2022년도 EL 하계 프로젝트
+ * IoT App조
+ * 주제 : 스마트 책상 & 의자
+ * 조원 : 조희우, 김신영, 김준호, 김진희, 김혜현
+ *
+ * 코드 제작 : 김진희
+ * 
+ * 기능 :
+ * - 공기 온습도 측정
+ *   - LCD에 측정 온도, 습도 값 표시
+ * - 공기질 (이산화탄소) 측정
+ *   - 측정값이 기준치 이상 올라갈 시 LED 점등
+ * - 가습기 모듈 작동
+ *   - 자동/수동 버튼 제어
+ *   - 자동 : 온습도 값에 따른 자동 작동
+ * - 가습기 모듈 원격 제어
+ * 
+ * 사용 부품 : 
+ *  - 보드 : Arduino R3
+ *  - 블루투스 모듈 : HC-06
+ *  - 온습도 센서 : DHT-11
+ *  - 공기질 센서 : SZH-SSBH-038
+ *  - 가습기 모듈 : SZH-HUM002
+ *  - 모스펫 : Irf520
+ *  - LCD : 1602 IC2
+ *  - LED : red
+ *  - 버튼 
+*/
+
+
 #include "DHT.h"
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
 
 LiquidCrystal_I2C lcd(0x27,16,2); //2004 -> 1602
 
@@ -12,6 +44,11 @@ DHT dht(DHTPIN, DHTTYPE);
 #define btn_on 3
 #define btn_off 4
 #define btn 7
+
+int bluetoothTx = 8;
+int bluetoothRx = 9;
+
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 
 int humi_max = 75;
 int humi_min = 50;
@@ -25,6 +62,7 @@ int b;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); //아두이노의 결과를 PC에 출력하겠다!
+  bluetooth.begin(9600);
   dht.begin();
   lcd.init();
   lcd.backlight();
@@ -38,6 +76,31 @@ void setup() {
 }
 
 void loop() {
+ //블루투스 값 입력
+ String inputString="";
+ 
+ if(bluetooth.available()){
+  inputString=bluetooth.readStringUntil('/n');
+ }
+
+ //가습기 자동 수동
+ if(inputString=="auto_on"){
+  a+=1;
+ }
+ else if(inputString=="auto_off"){
+  a+=1;
+ }
+ 
+ //가습기 모듈 on/off
+ if (inputString=="h_on"){
+  b+=1;
+ }
+ else if(inputString=="h_off"){
+  b+=1;
+ }
+  
+
+  
   int avrPPM = averageAnalogRead(A0); 
   int co2ppm = map(avrPPM, 0, 1023, 400, 5000);
   Serial.println(co2ppm);
@@ -58,6 +121,7 @@ void loop() {
 
   if(a%2==0){
     Serial.println("수동모드");
+    
     if(digitalRead(btn_on) == HIGH){
       //버튼이 눌려진것!
         b+=1;
